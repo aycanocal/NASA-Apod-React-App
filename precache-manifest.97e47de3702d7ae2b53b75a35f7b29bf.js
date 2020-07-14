9fbdd89109246de41e84dd2a1bf264b39ecd4752
AycanOcal <aycanocal28@gmail.com>
2020-07-14 11:49:42 +0300

    Updates

---
+++ precache-manifest.97e47de3702d7ae2b53b75a35f7b29bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f92b81595b981c38de5ddf274c3b02d",
+    "url": "/aycanocal/NASA-Apod-React-App/index.html"
+  },
+  {
+    "revision": "b84df3c94196bb183f8e",
+    "url": "/aycanocal/NASA-Apod-React-App/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "f9245c654d6bcd0e9bfd",
+    "url": "/aycanocal/NASA-Apod-React-App/static/css/main.86d9f089.chunk.css"
+  },
+  {
+    "revision": "b84df3c94196bb183f8e",
+    "url": "/aycanocal/NASA-Apod-React-App/static/js/2.6544134c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/aycanocal/NASA-Apod-React-App/static/js/2.6544134c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9245c654d6bcd0e9bfd",
+    "url": "/aycanocal/NASA-Apod-React-App/static/js/main.ce1c9675.chunk.js"
+  },
+  {
+    "revision": "8ce302da7a2ff915e070",
+    "url": "/aycanocal/NASA-Apod-React-App/static/js/runtime-main.7cab5c9d.js"
+  }
+]);
