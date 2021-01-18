d86fb97c40a038cbbfdd139761d5d7c915eb32c5
aycanocal <aycanocal28@gmail.com>
2021-01-19 00:44:11 +0300

    Updates

--- precache-manifest.3f1a18cb983501235f50a4a3c2ea278a.js
+++ precache-manifest.3f1a18cb983501235f50a4a3c2ea278a.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "674644217029596f14d376dbe0de6b3b",
+    "revision": "35d5ebc55735773da3741f6c051f55bc",
     "url": "/NASA-Apod-React-App/index.html"
   },
   {
-    "revision": "25ce4c3b2022abc15c35",
+    "revision": "10f162fb8c2ed605a4e8",
     "url": "/NASA-Apod-React-App/static/css/2.25ff2e5b.chunk.css"
   },
   {
-    "revision": "d325a5cf05c93ceaa5bd",
-    "url": "/NASA-Apod-React-App/static/css/main.36d22add.chunk.css"
+    "revision": "1fc2f3b0b19d4f257804",
+    "url": "/NASA-Apod-React-App/static/css/main.ed8708bb.chunk.css"
   },
   {
-    "revision": "25ce4c3b2022abc15c35",
+    "revision": "10f162fb8c2ed605a4e8",
     "url": "/NASA-Apod-React-App/static/js/2.fdfaac9d.chunk.js"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/NASA-Apod-React-App/static/js/2.fdfaac9d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d325a5cf05c93ceaa5bd",
-    "url": "/NASA-Apod-React-App/static/js/main.9a812cb1.chunk.js"
+    "revision": "1fc2f3b0b19d4f257804",
+    "url": "/NASA-Apod-React-App/static/js/main.125fe9d9.chunk.js"
   },
   {
     "revision": "7afb9387a4f96f9f7369",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NASA-Apod-React-App/precache-manifest.535419e514977feb82f3ddff40c201a0.js"
+  "/NASA-Apod-React-App/precache-manifest.3f1a18cb983501235f50a4a3c2ea278a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NASA-Apod-React-App/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
